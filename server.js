ca06404ed0111a136c008f5f0ab878ec1d2b0319
michael1996 <michael19_96@hotmail.nl>
2018-04-23 14:21:44 +0200

    commited version

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const application = express();
+const port = 8080;
+
+application.all('*', (request, response, next) =>{
+   console.log(request.url);
+   next();
+});
+
+application.listen(port, () => {
+   console.log('magic happens at port ' + port);
+});
+
+module.exports = application;
